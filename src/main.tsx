5c19174fc975202a923d171b788d41a75835a21f
rafael <rafaelandeloci67@gmail.com>
2023-10-24 17:27:29 -0300

    Custom hook to fetch products

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,30 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { RootLayout } from './root'
 import { Index } from './routes/_index'
 import { Checkout } from './routes/checkout'
 import { Order } from './routes/order'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RootLayout>
-      <Index />
-    </RootLayout>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Index />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/order" element={<Order />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 )
