16d23defa18dc5713bd040cfae0fbead7ff6dd0a
rafael <rafaelandeloci67@gmail.com>
2023-10-26 18:51:45 -0300

    Components properties change

--- src/components/quantity.tsx
+++ src/components/quantity.tsx
@@ -1,29 +1,33 @@
 import { Minus, Plus } from '@phosphor-icons/react'
+import { useState } from 'react'
 
-export function Quantity() {
+export function Quantity({ defaultValue = 1 }: { defaultValue?: number }) {
+  const [quantity, useQuantity] = useState(defaultValue)
   return (
     <div className="inline-flex items-center gap-1 rounded-md bg-base-button p-1">
       <button
         type="button"
         className="bg-transparent rounded p-1 leading-none text-purple disabled:opacity-30"
         aria-label="Diminuir quantidade"
+        onClick={() => useQuantity(quantity - 1)}
       >
         <Minus size={14} />
       </button>
 
       <span className="text-base tabular-nums leading-none text-base-title">
-        1
+        {quantity}
       </span>
 
       <button
         type="button"
         className="bg-transparent rounded p-1 leading-none text-purple disabled:opacity-30"
         aria-label="Aumentar quantidade"
+        onClick={() => useQuantity(quantity + 1)}
       >
         <Plus size={14} />
       </button>
 
-      <input type="hidden" name="quantity" defaultValue={1} />
+      <input type="hidden" name="quantity" defaultValue={quantity} />
     </div>
   )
 }
