1d349a8d8d6555a3aaa661dc4272bee0269828cf
rafael <rafaelandeloci67@gmail.com>
2023-10-26 18:52:43 -0300

    Misc

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,20 +8,13 @@ const variants = {
   icon: 'p-2 text-[1.375rem] text-base-card bg-purple-dark hover:bg-purple',
 }
 
-interface ButtonProps extends React.ComponentProps<'button'> {
-  variant?: keyof typeof variants
-}
-
-export function Button({
-  variant = 'primary',
-  className,
-  ...props
-}: ButtonProps) {
+export function Button({ variant = 'primary', className, ...props }: any) {
   return (
     <button
       className={cn(
         'inline-flex shrink-0 items-center justify-center gap-1 rounded-md uppercase transition-colors',
         'aria-busy:pointer-events-none aria-busy:cursor-wait aria-busy:opacity-60',
+        // @ts-ignore Vamos arrumar isso depois
         variants[variant],
         className
       )}
--- src/components/quantity.tsx
+++ src/components/quantity.tsx
@@ -1,33 +1,29 @@
 import { Minus, Plus } from '@phosphor-icons/react'
-import { useState } from 'react'
 
-export function Quantity({ defaultValue = 1 }: { defaultValue?: number }) {
-  const [quantity, useQuantity] = useState(defaultValue)
+export function Quantity() {
   return (
     <div className="inline-flex items-center gap-1 rounded-md bg-base-button p-1">
       <button
         type="button"
         className="bg-transparent rounded p-1 leading-none text-purple disabled:opacity-30"
         aria-label="Diminuir quantidade"
-        onClick={() => useQuantity(quantity - 1)}
       >
         <Minus size={14} />
       </button>
 
       <span className="text-base tabular-nums leading-none text-base-title">
-        {quantity}
+        1
       </span>
 
       <button
         type="button"
         className="bg-transparent rounded p-1 leading-none text-purple disabled:opacity-30"
         aria-label="Aumentar quantidade"
-        onClick={() => useQuantity(quantity + 1)}
       >
         <Plus size={14} />
       </button>
 
-      <input type="hidden" name="quantity" defaultValue={quantity} />
+      <input type="hidden" name="quantity" defaultValue={1} />
     </div>
   )
 }
